72953376362bc053ae211df4c986b83de0c6cbc4
Davitree <davidkhadka444@gmail.com>
2026-05-22 22:10:49 +0545

    completed exercise 37

--- part01-Part01_37.GiftTax/src/main/java/GiftTax.java
+++ part01-Part01_37.GiftTax/src/main/java/GiftTax.java
@@ -5,6 +5,25 @@ public class GiftTax {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        System.out.println("Value of gift?");
+        double giftValue = Double.valueOf(scan.nextLine());
+        if (giftValue < 5000) {
+            System.out.println("No tax!");
+        } else if (giftValue >= 5000 && giftValue < 25000) {
+            giftValue =   (100 + (giftValue - 5000) * 0.08);
+            System.out.println("Tax: " + giftValue);
+        } else if (giftValue >= 25000 && giftValue < 55000) {
+            giftValue =  (1700 + (giftValue - 25000) * 0.1);
+            System.out.println("Tax: " + giftValue);
+        } else if (giftValue >= 55000 && giftValue < 200000) {
+            giftValue =  (4700 + (giftValue - 55000) * 0.12);
+            System.out.println("Tax: " + giftValue);
+        } else if (giftValue >= 200000 && giftValue < 1000000) {
+            giftValue =  (22100 + (giftValue - 200000) * 0.15);
+            System.out.println("Tax: " + giftValue);
+        } else {
+            giftValue =  (142100 + (giftValue - 1000000) * 0.17);
+            System.out.println("Tax: " + giftValue);
+        }
     }
 }
